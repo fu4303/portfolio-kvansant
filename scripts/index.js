d8b58b157fa1f52718e729f64a359f4fed253ff9
Kristopher <kristopher@signaltransmit.com>
2019-09-18 14:42:32 -0700

    new
    New portfolio

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+// On hover hide section, display other
+
+function show(id) {
+  document.getElementById(id).style.visibility = "visible";
+  document.getElementById(id).style.opacity = "1";
+}
+function hide(id) {
+  document.getElementById(id).style.visibility = "hidden";
+  document.getElementById(id).style.opacity = "0";
+}
